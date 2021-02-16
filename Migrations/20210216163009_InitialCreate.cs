e4170ae64b3b456c51ab9b8f87a0d45dc1529a57
weihenglim <imweihengsit@gmail.com>
2021-02-17 00:32:05 +0800

    Initial db migration

---
+++ Migrations/20210216163009_InitialCreate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _2106Proj.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GuestId = table.Column<int>(nullable: false),
+                    DueDate = table.Column<DateTime>(nullable: false),
+                    PaymentMethod = table.Column<string>(nullable: true),
+                    Amount = table.Column<decimal>(nullable: false),
+                    Discriminator = table.Column<string>(nullable: false),
+                    RoomId = table.Column<int>(nullable: true),
+                    ReservationId = table.Column<int>(nullable: true),
+                    IssueDate = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payment", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReceiptItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Quantity = table.Column<int>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    PostChargeId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReceiptItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReceiptItem_Payment_PostChargeId",
+                        column: x => x.PostChargeId,
+                        principalTable: "Payment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceiptItem_PostChargeId",
+                table: "ReceiptItem",
+                column: "PostChargeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReceiptItem");
+
+            migrationBuilder.DropTable(
+                name: "Payment");
+        }
+    }
+}
