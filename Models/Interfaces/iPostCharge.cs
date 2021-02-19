996d598cb359af87a65df524444a577560740bed
weihenglim <imweihengsit@gmail.com>
2021-02-19 21:08:59 +0800

    Implement iPostCharge and iReservationInvoice interfaces

---
+++ Models/Interfaces/iPostCharge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2106Proj.Models
+{
+    interface iPostCharge
+    {
+        public IEnumerable<PostCharge> getAllCharges();
+        public IEnumerable<PostCharge> getChargeByResID(int id);
+        public PostCharge getChargeByID(int id);
+        public PostCharge createNewCharge(int roomID, int ID, DateTime due, string method, decimal amount, int guestID);
+        public void updateCharge(PostCharge Charge);
+        public void deleteCharge(int id);
+        public void addItem(int id, ReceiptItem item);
+        public void removeItem(int id, int itemID);
+        public decimal calculateTotal();
+    }
+}
---
+++ Models/Interfaces/iReservationInvoice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2106Proj.Models
+{
+    interface iReservationInvoice
+    {
+        public IEnumerable<ReservationInvoice> getAllInvoices();
+        public IEnumerable<ReservationInvoice> getInvoiceByResID(int id);
+        public ReservationInvoice getInvoiceByID(int id);
+        public ReservationInvoice createNewInvoice(int resID, DateTime issue, int ID, DateTime due, string method, decimal amount, int guestID);
+        public void updateInvoice(ReservationInvoice invoice);
+        public void deleteInvoice(int id);
+    }
+}
