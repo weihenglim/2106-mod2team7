0dcc1e7dbd5fb07aab08af789a6137d8cd7d51e8
weihenglim <imweihengsit@gmail.com>
2021-02-19 22:43:31 +0800

    Add PostChargeControl class

---
+++ Models/Controls/PostChargeControl.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using _2106Proj.Data;
+
+namespace _2106Proj.Models.Controls
+{
+    public class PostChargeControl : iPostCharge
+    {
+        private readonly PostChargeGateway _chargeGateway;
+        public PostChargeControl(PostChargeGateway chargeGateway)
+        {
+            _chargeGateway = chargeGateway;
+        }
+
+        public void addItem(int id, ReceiptItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal calculateTotal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PostCharge createNewCharge(int roomID, int ID, DateTime due, string method, decimal amount, int guestID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteCharge(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PostCharge> getAllCharges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PostCharge getChargeByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PostCharge> getChargeByResID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removeItem(int id, int itemID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateCharge(PostCharge Charge)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
